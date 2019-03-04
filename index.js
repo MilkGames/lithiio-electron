1562d91e9cb32981b255bb54caaebba99cca4dca
MilkGames <memegames1337@gmail.com>
2019-03-04 23:23:17 +0100

    Fix flashing window, add icon

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const { app, BrowserWindow, shell} = require('electron')
 
 function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600, nodeIntegration: true, frame: true });
-  win.loadFile('./www/setup.html')
-  win.maximize();
-  win.setMenuBarVisibility(false);
+	win = new BrowserWindow({ width: 800, height: 600, nodeIntegration: true, frame: true, show: false, icon: './build/icon.png' });
+	win.loadFile('./www/setup.html')
+	win.once('ready-to-show', () => {
+		win.show()
+		win.maximize();
+		win.setMenuBarVisibility(false);
+	}) 
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  app.quit();
+	app.quit();
 });
