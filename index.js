b72ee7b135a235d81e8343919e483d0dff70dcab
MilkGames <memegames1337@gmail.com>
2019-03-28 21:17:31 +0100

    Fix Icon, add hide/show in tray option, show changelog for updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ autoUpdater.autoDownload = false
 const path = require('path');
 app.on('ready', function(){
 	autoUpdater.checkForUpdatesAndNotify();
-	win = new BrowserWindow({ width: 800, height: 600, nodeIntegration: true, frame: true, show: false, icon: './build/icon.png' });
+	win = new BrowserWindow({ width: 800, height: 600, nodeIntegration: true, frame: true, show: false, icon: path.join(__dirname, '/build/icon.png') });
 	win.loadFile('./www/setup.html')
 	win.once('ready-to-show', () => {
 		win.show();
@@ -13,12 +13,9 @@ app.on('ready', function(){
 	});
 	tray = new Tray(nativeImage.createFromPath(path.join(__dirname, '/build/icon.png')));
 	var contextMenu = Menu.buildFromTemplate([
-		/*{ click: function(){
+		{ click: function(){
 			win.isVisible() ? win.hide() : win.show();
-			console.log(contextMenu)
-			contextMenu.getMenuItemById('hideshow').label = win.isVisible() ? 'Hide in tray' : 'Show'; // Oh yeah baby
-			tray.setContextMenu(contextMenu); // For the Linux bois
-		}, label: 'EPIC GAMER MOMENT', type: 'normal', id: 'hideshow' }, SOMEONE PLEASE FIX THIS OH GOD HELP ME*/
+		}, label: 'Show/Hide in tray', type: 'normal' },
 		{ click: function(){
 			win.webContents.executeJavaScript('openUp();');
 		}, label: 'Upload file(s)', type: 'normal' },
@@ -42,27 +39,28 @@ autoUpdater.on('error', (error) => {
   dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
 })
 
-autoUpdater.on('update-available', () => {
+autoUpdater.on('update-available', (info) => {
   dialog.showMessageBox({
     type: 'info',
     title: 'Found Updates',
-    message: 'Found updates, do you want update now?',
+    message: 'Found updates, do you want update now?\n\nRelease notes:' + info.releaseNotes.replace(/(<([^>]+)>)/ig, ""), // 
     buttons: ['No', 'Sure']
   }, (buttonIndex) => {
     if (buttonIndex === 1) {
       autoUpdater.downloadUpdate()
     }
     else {
-      autoUpdater.enabled = true
-      autoUpdater = null
+      autoUpdater.enabled = false;
     }
   })
 })
 
 autoUpdater.on('update-downloaded', () => {
   dialog.showMessageBox({
+  	type: 'info',
     title: 'Install Updates',
-    message: 'Updates downloaded, restarting for update...'
+    message: 'Updates downloaded, restarting for update...',
+    buttons: ['Restart']
   }, () => {
     setImmediate(() => autoUpdater.quitAndInstall())
   })
