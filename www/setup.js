1230ddcd7117268cf00561a3310979aa24ba397b
MilkGames <memegames1337@gmail.com>
2019-07-10 02:45:06 +0200

    Make setup/login work (Loader on sign in button!)

--- www/setup.js
+++ www/setup.js
@@ -4,6 +4,7 @@ if (localStorage.getItem("apikey")) {
 const {shell} = require('electron').remote;
 const lithiio = require('node-lithiio-upload');
 function login() {
+	document.getElementById("loginb").classList.add("loading")
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
 	lithiio.fetchAPIKey(email, password).once('success', function(apikey) {
@@ -11,6 +12,7 @@ function login() {
 		window.location.replace("index.html");
 	}).once('error', function(error) { // If there's an error fetching the API Key
 	document.getElementById("error").innerHTML = error;
+	document.getElementById("loginb").classList.remove("loading")
 });
 };
 document.getElementById("password").addEventListener("keyup", function(event) {
